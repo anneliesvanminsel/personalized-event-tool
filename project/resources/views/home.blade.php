60f7021f155fd8784716dc3491d4cb1542047dd7
Annelies <anneliesvanminsel@outlook.com>
2019-12-02 16:27:56 +0100

    Added startpage

---
+++ project/resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.masterlayout')
+@section('title')
+    Eventify__
+@endsection
+@section('content')
+
+@endsection
---
+++ project/resources/views/partials/header.blade.php
@@ -0,0 +1,9 @@
+<header class="sidebar">
+    <nav class="nav">
+        <ul class="nav__bar">
+            <li class="nav__item">
+                <a class="nav__link" href="">item A</a>
+            </li>
+        </ul>
+    </nav>
+</header>
