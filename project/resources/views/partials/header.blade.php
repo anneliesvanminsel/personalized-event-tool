df6b727d6504d24dd6202a244dbb7c1ae4e4826c
Annelies <anneliesvanminsel@outlook.com>
2019-12-02 20:23:12 +0100

    added header with basic design

--- project/resources/views/partials/header.blade.php
+++ project/resources/views/partials/header.blade.php
@@ -1,8 +1,17 @@
-<header class="sidebar">
+<header class="header">
+    <div class="logo">
+        Eventify__
+    </div>
     <nav class="nav">
         <ul class="nav__bar">
             <li class="nav__item">
-                <a class="nav__link" href="">item A</a>
+                <a class="nav__link" href="">evenementen</a>
+            </li>
+            <li class="nav__item">
+                <a class="nav__link" href="">voor organisatoren</a>
+            </li>
+            <li class="nav__item">
+                <a class="nav__link" href="">aanmelden</a>
             </li>
         </ul>
     </nav>
