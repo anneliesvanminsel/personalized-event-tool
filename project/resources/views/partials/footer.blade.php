60e5f9ce94e994829e046cf2adc766a9b8b2afa5
Annelies <anneliesvanminsel@outlook.com>
2019-12-02 20:22:36 +0100

    added footer with basic design

---
+++ project/resources/views/partials/footer.blade.php
@@ -0,0 +1,54 @@
+<footer class="footer">
+	<div class="logo">
+		Eventify__
+	</div>
+	<div class="row row--stretch">
+		<ul class="list">
+			<li class="list__item">
+				bedrijfsnaam
+			</li>
+			<li class="list__item">
+				bedrijfstraat 45
+			</li>
+			<li class="list__item">
+				3000 Leuven
+			</li>
+			<li class="list__item">
+				+32 456 78 92 34
+			</li>
+			<li class="list__item">
+				bedrijf@mail.be
+			</li>
+		</ul>
+
+		<ul class="list">
+			<li class="list__item">
+				Organisatoren
+			</li>
+			<li class="list__item">
+				<a class="list__link" href="">overzicht</a>
+			</li>
+			<li class="list__item">
+				<a class="list__link" href="">onze features</a>
+			</li>
+			<li class="list__item">
+				<a class="list__link" href="">prijzen</a>
+			</li>
+		</ul>
+
+		<ul class="list">
+			<li class="list__item">
+				Bezoekers
+			</li>
+			<li class="list__item">
+				<a class="list__link" href="">Zoek evenementen</a>
+			</li>
+			<li class="list__item">
+				<a class="list__link" href="">Bekijk mijn tickets</a>
+			</li>
+		</ul>
+
+	</div>
+
+
+</footer>
