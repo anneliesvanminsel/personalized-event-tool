ecf7032315a69b3adac0c83e5348f75b534f0e6e
Annelies <anneliesvanminsel@outlook.com>
2019-12-03 19:20:56 +0100

    some more design

--- project/resources/views/home.blade.php
+++ project/resources/views/home.blade.php
@@ -3,5 +3,13 @@
     Eventify__
 @endsection
 @section('content')
-
+    <section class="slideshow">
+        <img src="" alt="">
+        <h1>
+            Kleurenfeest
+        </h1>
+        <p>
+            Geniet mee van de spetterende kleuren.
+        </p>
+    </section>
 @endsection
