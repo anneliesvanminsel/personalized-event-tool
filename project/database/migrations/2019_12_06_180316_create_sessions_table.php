406660b5ee124465928f5aefc4b5bf749bf9d142
Annelies <anneliesvanminsel@outlook.com>
2019-12-06 20:48:37 +0100

    Added more models and migrations

---
+++ project/database/migrations/2019_12_06_180316_create_sessions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSessionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('name')->nullable();
+			$table->string('city');
+			$table->date('date');
+			$table->string('locationname')->nullable();
+			$table->number('totaltickets');
+			$table->string('grondplan')->nullable(); //TODO: moet een afbeelding worden
+			$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sessions');
+    }
+}
