d5b1994214eba290522f4ec7bfa37e0579a85aa2
Annelies <anneliesvanminsel@outlook.com>
2019-12-06 12:55:37 +0100

    foutje in d footer

--- project/resources/views/home.blade.php
+++ project/resources/views/home.blade.php
@@ -4,12 +4,25 @@
 @endsection
 @section('content')
     <section class="slideshow">
-        <img src="" alt="">
+        <div>
+            <img src="" alt="">
+        </div>
         <h1>
             Kleurenfeest
         </h1>
         <p>
             Geniet mee van de spetterende kleuren.
         </p>
+    </section>
+    <section>
+        <h1>
+            Ga op avontuur
+        </h1>
+        <div>
+            hier komen de filters
+        </div>
+        <div>
+            hierkomt de lijst met de verschillende events
+        </div>
     </section>
 @endsection
