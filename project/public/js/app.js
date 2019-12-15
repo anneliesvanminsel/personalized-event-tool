a66ec4b68271b3fd4ca01470af6feec35263d19e
Annelies <anneliesvanminsel@outlook.com>
2019-12-16 00:32:28 +0100

    fixed some stuff with babel

--- project/public/js/app.js
+++ project/public/js/app.js
@@ -68843,7 +68843,7 @@ __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
  */
 
 
-__webpack_require__(/*! ./components/Component */ "./resources/js/components/Component.jsx");
+__webpack_require__(/*! ./components/slideshow/Slider */ "./resources/js/components/slideshow/Slider.jsx");
 
 /***/ }),
 
@@ -68892,20 +68892,148 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
-/***/ "./resources/js/components/Component.jsx":
-/*!***********************************************!*\
-  !*** ./resources/js/components/Component.jsx ***!
-  \***********************************************/
+/***/ "./resources/js/components/slideshow/LeftArrow.jsx":
+/*!*********************************************************!*\
+  !*** ./resources/js/components/slideshow/LeftArrow.jsx ***!
+  \*********************************************************/
 /*! exports provided: default */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Example; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+
+
+var LeftArrow = function LeftArrow(props) {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "backArrow",
+    onClick: props.goToPrevSlide
+  }, "TERUG");
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (LeftArrow);
+
+/***/ }),
+
+/***/ "./resources/js/components/slideshow/RightArrow.jsx":
+/*!**********************************************************!*\
+  !*** ./resources/js/components/slideshow/RightArrow.jsx ***!
+  \**********************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+
+
+var RightArrow = function RightArrow(props) {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "nextArrow",
+    onClick: props.goToNextSlide
+  }, "VOLGENDE");
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (RightArrow);
+
+/***/ }),
+
+/***/ "./resources/js/components/slideshow/Slide.jsx":
+/*!*****************************************************!*\
+  !*** ./resources/js/components/slideshow/Slide.jsx ***!
+  \*****************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
 /* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_dom__WEBPACK_IMPORTED_MODULE_1__);
+
+
+/*
+class Slide extends Component {
+    render() {
+        return (
+            <div className="row row--stretch">
+                <div className="ctn--image">
+                    <img
+                        src="https://images.pexels.com/photos/801863/pexels-photo-801863.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                        alt=""
+                    />
+                </div>
+                <div className="hero__text">
+                    <h1>
+                        lala
+                    </h1>
+                    <p>
+                        sflkqsmldkfkqsfmlkqmldfklmqksdflm
+
+                    </p>
+                    <p>
+                        LSKDLKDFMQ
+                    </p>
+                    <a className="btn btn--white" href="#">
+                        Bekijk tickets
+                    </a>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Slide;
+*/
+
+var Slide = function Slide(_ref) {
+  var index = _ref.index,
+      image = _ref.image;
+  var styles = {
+    width: '100%',
+    height: '100%'
+  };
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "slide",
+    style: styles
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "hero__content row row--stretch"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "ctn--image"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    src: image,
+    alt: ""
+  })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "hero__text"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "lala"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "kdklkff"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "woepsiepoepsie"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    className: "btn btn--white",
+    href: "#"
+  }, "Bestel tickets"))));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Slide);
+
+/***/ }),
+
+/***/ "./resources/js/components/slideshow/Slider.jsx":
+/*!******************************************************!*\
+  !*** ./resources/js/components/slideshow/Slider.jsx ***!
+  \******************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
+/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_dom__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _Slide__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./Slide */ "./resources/js/components/slideshow/Slide.jsx");
+/* harmony import */ var _LeftArrow__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./LeftArrow */ "./resources/js/components/slideshow/LeftArrow.jsx");
+/* harmony import */ var _RightArrow__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./RightArrow */ "./resources/js/components/slideshow/RightArrow.jsx");
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -68916,54 +69044,122 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
 function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
 
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
 function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
 
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 
 
-var Example =
+
+
+
+
+var Slider =
 /*#__PURE__*/
 function (_Component) {
-  _inherits(Example, _Component);
+  _inherits(Slider, _Component);
 
-  function Example() {
-    _classCallCheck(this, Example);
+  function Slider(props) {
+    var _this;
 
-    return _possibleConstructorReturn(this, _getPrototypeOf(Example).apply(this, arguments));
+    _classCallCheck(this, Slider);
+
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(Slider).call(this, props));
+
+    _defineProperty(_assertThisInitialized(_this), "goToPrevSlide", function () {
+      if (_this.state.currentIndex === 0) {
+        return _this.setState({
+          currentIndex: 0,
+          translateValue: 0
+        });
+      } // This will not run if we met the if condition above
+
+
+      _this.setState(function (prevState) {
+        return {
+          currentIndex: prevState.currentIndex - 1,
+          translateValue: prevState.translateValue + _this.slideWidth()
+        };
+      });
+
+      console.log('prev slide', _this.state.currentIndex);
+    });
+
+    _defineProperty(_assertThisInitialized(_this), "goToNextSlide", function () {
+      // Exiting the method early if we are at the end of the images array.
+      // We also want to reset currentIndex and translateValue, so we return
+      // to the first image in the array.
+      if (_this.state.currentIndex === _this.state.images.length - 1) {
+        return _this.setState({
+          currentIndex: 0,
+          translateValue: 0
+        });
+      } // This will not run if we met the if condition above
+
+
+      _this.setState(function (prevState) {
+        return {
+          currentIndex: prevState.currentIndex + 1,
+          translateValue: prevState.translateValue + -_this.slideWidth()
+        };
+      });
+
+      console.log('next slide');
+    });
+
+    _defineProperty(_assertThisInitialized(_this), "slideWidth", function () {
+      console.log(document.querySelector('.slide').clientWidth);
+      return document.querySelector('.slide').clientWidth;
+    });
+
+    _this.state = {
+      images: ["https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/aurora.jpg", "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/canyon.jpg", "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/city.jpg", "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/desert.jpg", "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/mountains.jpg", "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/redsky.jpg", "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/sandy-shores.jpg", "https://s3.us-east-2.amazonaws.com/dzuz14/thumbnails/tree-of-life.jpg"],
+      currentIndex: 0,
+      translateValue: 0
+    };
+    return _this;
   }
 
-  _createClass(Example, [{
+  _createClass(Slider, [{
     key: "render",
     value: function render() {
       return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "container"
+        className: "hero"
       }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "row justify-content-center"
+        className: "hero__content"
       }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "col-md-8"
-      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "card"
-      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "card-header"
-      }, "Example Component"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-        className: "card-body"
-      }, "I'm an example component!")))));
+        className: "slider-wrapper row row--stretch",
+        style: {
+          transform: "translateX(".concat(this.state.translateValue, "px)"),
+          transition: 'transform ease-out 0.45s',
+          width: '100%'
+        }
+      }, this.state.images.map(function (image, i) {
+        return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_Slide__WEBPACK_IMPORTED_MODULE_2__["default"], {
+          key: i,
+          image: image
+        });
+      }))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_LeftArrow__WEBPACK_IMPORTED_MODULE_3__["default"], {
+        goToPrevSlide: this.goToPrevSlide
+      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_RightArrow__WEBPACK_IMPORTED_MODULE_4__["default"], {
+        goToNextSlide: this.goToNextSlide
+      }));
     }
   }]);
 
-  return Example;
+  return Slider;
 }(react__WEBPACK_IMPORTED_MODULE_0__["Component"]);
 
+/* harmony default export */ __webpack_exports__["default"] = (Slider);
 
-
-if (document.getElementById('example')) {
-  react_dom__WEBPACK_IMPORTED_MODULE_1___default.a.render(react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(Example, null), document.getElementById('example'));
+if (document.getElementById('event-slider')) {
+  react_dom__WEBPACK_IMPORTED_MODULE_1___default.a.render(react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(Slider, null), document.getElementById('event-slider'));
 }
 
 /***/ }),
