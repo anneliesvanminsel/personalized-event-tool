b09e0ef57396e4ba30f5e2700915a91eaaf40aae
Annelies <anneliesvanminsel@outlook.com>
2019-12-21 00:30:13 +0100

    trying to fix something

--- project/app/Http/Controllers/OrganisationController.php
+++ project/app/Http/Controllers/OrganisationController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\Event;
 
 class OrganisationController extends Controller
 {
     //
 	public function getDashboard($org_id) {
 		$user = User::where('id', $org_id)->first();
-		return view('content.org.dashboard', ['user' => $user]);
+		$events = Event::whre('organisation_id', $org_id)->paginate(5);
+
+		return view('content.org.dashboard', ['user' => $user, 'events' => $events]);
 	}
 }
