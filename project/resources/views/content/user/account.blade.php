6ac9d1e63a191f04f99d3173b99fab2cf311ee6f
Annelies <anneliesvanminsel@outlook.com>
2020-01-09 13:51:20 +0100

    fix view liked events

--- project/resources/views/content/user/account.blade.php
+++ project/resources/views/content/user/account.blade.php
@@ -8,10 +8,23 @@
 			<h1>
 				Account van {{ $user['name'] }}
 			</h1>
+			<div class="row row--stretch">
+				@if($user->tickets()->exists())
+					<a href="#tickets" class="btn">Mijn tickets</a>
+				@endif
+				
+				@if($user->shifts()->exists())
+					<a href="#shifts" class="btn">Mijn shifts</a>
+				@endif
+				
+				@if($user->events()->exists())
+					<a href="#events" class="btn">Mijn evenementen</a>
+				@endif
+			</div>
 		</section>
 		
 		@if($user->tickets()->exists())
-			<section class="spacing-top-m">
+			<section class="spacing-top-m" id="tickets">
 				<h2>
 					Mijn tickets
 				</h2>
@@ -49,7 +62,7 @@
 		@endif
 		
 		@if($user->shifts()->exists())
-			<section class="spacing-top-m">
+			<section class="spacing-top-m" id="shifts">
 				<h2>
 					Mijn taken en shiften
 				</h2>
@@ -60,6 +73,33 @@
 				</div>
 			</section>
 		@endif
-	
+		
+		@if($user->events()->exists())
+			<section class="spacing-top-m" id="events">
+				<h2>
+					Mijn evenementen
+				</h2>
+				<div class="h-grid">
+					@foreach($user->events()->get() as $event)
+						<a
+								class="row row--stretch h-grid__item item"
+								href=""
+						>
+							<div class="item__date bkg-red">
+								{{  date('d M', strtotime( $event['starttime'])) }}
+							</div>
+							
+							<div class="is-grow">
+								<p class="item__title item__text">{{ $event->title }}</p>
+								<p class="item__text">{{ $event->description }}</p>
+							</div>
+							<div class="ctn--image item__image">
+								<img src="{{ asset('images/' . $event['logo'] ) }}" alt="{{ $event['title'] }}" loading="lazy">
+							</div>
+						</a>
+					@endforeach
+				</div>
+			</section>
+		@endif
 	</div>
 @endsection
