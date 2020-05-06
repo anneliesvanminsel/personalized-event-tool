8c131eb557d73042fd4f25e5356e88391ec2291b
Annelies <anneliesvanminsel@outlook.com>
2020-05-06 17:01:25 +0200

    added links to list-events

--- project/resources/views/partials/listitem.blade.php
+++ project/resources/views/partials/listitem.blade.php
@@ -1,27 +1,30 @@
 <div class="list__item item row row--stretch">
 	<div class="item__date">
-		<div class="date-field">
-			<div>
-				@if($event['endtime'])
-					{{ \Jenssegers\Date\Date::parse(strtotime($event['starttime']))->format('j') }} -
-					{{ \Jenssegers\Date\Date::parse(strtotime($event['endtime']))->format('j') }}
-				@else
-					{{ \Jenssegers\Date\Date::parse(strtotime($event['starttime']))->format('j') }}
-				@endif
+		<a href={{ route('event.detail', ['event_id' => $event->id]) }}>
+			<div class="date-field">
+				<div>
+					@if($event['endtime'])
+						{{ \Jenssegers\Date\Date::parse(strtotime($event['starttime']))->format('j') }} -
+						{{ \Jenssegers\Date\Date::parse(strtotime($event['endtime']))->format('j') }}
+					@else
+						{{ \Jenssegers\Date\Date::parse(strtotime($event['starttime']))->format('j') }}
+					@endif
+				</div>
 			</div>
-		</div>
-		<div class="date-month">
-			{{ \Jenssegers\Date\Date::parse(strtotime($event['starttime']))->format('M') }}
-		</div>
+			<div class="date-month">
+				{{ \Jenssegers\Date\Date::parse(strtotime($event['starttime']))->format('M') }}
+			</div>
+		</a>
 	</div>
-	
-	
 	
 	<div class="item__content">
 		<div class="item__section row row--stretch">
 			<h4 class="item__title">
-				{{ $event->title }}
+				<a href={{ route('event.detail', ['event_id' => $event->id]) }}>
+					{{ $event->title }}
+				</a>
 			</h4>
+			
 			<div class="item__actions row">
 				<button title="download" class="button is-icon" type="submit">
 					@svg('download', 'is-btn')
@@ -43,7 +46,6 @@
 					@endif
 				</form>
 			</div>
-			
 		</div>
 		
 		<div class="item__section row">
@@ -56,21 +58,20 @@
 				Leuven
 			</div>
 		</div>
-		
 		<div class="item__text item__section">
-			{{ $event->description }}
+			<a href={{ route('event.detail', ['event_id' => $event->id]) }}>
+				{{ $event->description }}
+			</a>
 		</div>
-		
-		
 	</div>
 	
-	@if(File::exists(public_path() . "/images/" . $event['logo']))
-		<div class="item__image ctn--image">
-			<img src="{{ asset('images/' . $event['logo'] ) }}" alt="{{ $event['title'] }}" loading="lazy">
-		</div>
-	@else
-		<div class="item__image ctn--image">
-			<img src="https://placekitten.com/600/600" alt="{{ $event['title'] }}" loading="lazy">
-		</div>
-	@endif
+	<div class="item__image ctn--image">
+		<a href={{ route('event.detail', ['event_id' => $event->id]) }}>
+			@if(File::exists(public_path() . "/images/" . $event['logo']))
+				<img src="{{ asset('images/' . $event['logo'] ) }}" alt="{{ $event['title'] }}" loading="lazy">
+			@else
+				<img src="https://placekitten.com/600/600" alt="{{ $event['title'] }}" loading="lazy">
+			@endif
+		</a>
+	</div>
 </div>
